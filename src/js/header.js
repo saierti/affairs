4b1b439aec3fad5dfcb3e0e79c0b31386651aacc
saierti <310320717@qq.com>
2018-07-25 17:08:28 +0800

    greed

---
+++ src/js/header.js
@@ -0,0 +1,24 @@
+
+$('document').ready(function(){
+    var a = $('header p');
+    /*初始化状态下显示待完成 */
+    $(a[0]).next().css("width","20%");
+    /*重置另一个图标的样式 */
+    function reset(e){
+        e.css('font-size','4vw');
+        e.css("color","#ffffff");
+        e.next().css("width","0");
+    }
+    $(a[0]).click(function(){
+        reset($(a[1]));
+        $(a[0]).css("font-size","5vw");
+        $(a[0]).css("color","#fbf6c1");
+        $(a[0]).next().css("width","20%");
+    })
+    $(a[1]).click(function(){
+        reset($(a[0]));
+        $(a[1]).css("font-size","5vw");
+        $(a[1]).css("color","#fbf6c1");
+        $(a[1]).next().css("width","20%");
+    })
+})
