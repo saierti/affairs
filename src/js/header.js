9ab610af563651bacf90c68dc0211a2ffaf40177
saierti <310320717@qq.com>
2018-07-25 17:18:50 +0800

    greed

--- src/js/header.js
+++ src/js/header.js
@@ -5,19 +5,19 @@ $('document').ready(function(){
     $(a[0]).next().css("width","20%");
     /*重置另一个图标的样式 */
     function reset(e){
-        e.css('font-size','4vw');
+        e.css('font-size','4.5vw');
         e.css("color","#ffffff");
         e.next().css("width","0");
     }
     $(a[0]).click(function(){
         reset($(a[1]));
-        $(a[0]).css("font-size","5vw");
+        $(a[0]).css("font-size","5.2vw");
         $(a[0]).css("color","#fbf6c1");
         $(a[0]).next().css("width","20%");
     })
     $(a[1]).click(function(){
         reset($(a[0]));
-        $(a[1]).css("font-size","5vw");
+        $(a[1]).css("font-size","5.2vw");
         $(a[1]).css("color","#fbf6c1");
         $(a[1]).next().css("width","20%");
     })
