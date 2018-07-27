ed8fefa7958ab150c1b1b8f20905fa216cfbe218
Crazy492 <1056872781@qq.com>
2018-07-27 09:05:28 +0800

    0.0

--- dest/js/index.js
+++ dest/js/index.js
@@ -1,7 +1,81 @@
 $('document').ready(function(){
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /****************************************** */
+
     $('.img4').parent().css('color','black');
     var  aA = $('#footerFlex > a');
+
+
+
+
+    /**************************/ 
+    var oHead2 = $('#header2')
+    var oHead3 = $('#header3')
+    var oDiv1 = $('#part1')
+    var oDiv2 = $('#part2')
+    var oDiv3 = $('#part3')
+    $(aA[1]).click(function(){
+        {
+            $(oHead3).css('display','block');
+            $(oDiv3).css('display','block');
+            $(oHead2).css('display','none');
+            $(oDiv2).css('display','none');
+            $(oDiv1).css('display','none');
+        }
+    })
+    $('.div1').click(function(){
+        {
+            $(oHead3).css('display','none');
+            $(oDiv3).css('display','none');
+            $(oHead2).css('display','block');
+            $(oDiv2).css('display','block');
+            $(oDiv1).css('display','none');
+        }
+    })
+    $(aA[4]).click(function(){
+        {
+            $(oHead3).css('display','none');
+            $(oDiv3).css('display','none');
+            $(oHead2).css('display','none');
+            $(oDiv2).css('display','none');
+            $(oDiv1).css('display','block');
+        }
+    })
+
+    /***************************/ 
 
     aA.click(function(ev){
 
@@ -45,7 +119,13 @@ $('document').ready(function(){
 
         }
     })
+    /************************* */
 })
+
+
+
+
+
 
     
 
