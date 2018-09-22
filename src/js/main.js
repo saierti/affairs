fffc34ea4fee81e74e8258b70cb4213ce26123e4
saierti <310320717@qq.com>
2018-09-22 12:14:13 +0800

    9.22刚过来蒲公英

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,100 @@
 $('document').ready(function(){
-    //交互
-    http.post('/fnc/unclaimed',{},function(res){
-        for(let i=0;i < res.Evn.length;i++){
-            var a = parseInt(res.Evn[i].date);
-            var time = new Date(a);
-            var tag =res.Evn[i].tag;
-            var month =time.getMonth();
-            var day =time.getDay();
-            var hour = time.getHours();
-            var minutes = time.getMinutes();
-            var describe =res.Evn[i].describe;
-            var eventId = res.Evn[i].eventId;
+    
+    http.post('/fnc/unclaimed',{
+        
+    },res => {
+        console.log('x',res);
+        res.Evn.forEach(e => {
+            var time = new Date(parseInt(e.date));
+
             var cardbox = `
             <div class="mainbox">
                 <div class="blackbox"></div>
-                <div class="mainImg" data-id = "${eventId}">
+                <div class="mainImg" data-id = "${e.eventId}">
                     <div class="mainboxtop">
-                        <div class="label">${tag}</div>
-                        <div class="date">${month}月${day}日</div>
-                        <div class="time">${hour}:${minutes}</div>
+                        <div class="label">${e.tag}</div>
+                        <div class="date">${time.getMonth()}月${time.getDay()}日</div>
+                        <div class="time">${time.getHours()}:${time.getMinutes()}</div>
                     </div>
                     <div class="mainboxmid">
-                        <p>${describe}</p>
+                        <p>${e.describe}</p>
                     </div>
                 </div>
     
                 <div class="mainboxbottom">
                     <div class="mainhead"><div class="headimg"></div><span>jio雯婷</span></span></div>
-                    <div class="btnPick" data-id = "${eventId}" data-like ="0"></div>
+                    <div class="btnPick" data-id = "${e.eventId}" data-like ="0"></div>
                     <div class="btnDiscuss"></div>
                 </div>
             </div>`;
-            $('#mainbefore').after(cardbox);
-            console.log(eventId);
-            //获取刷新页面后点赞的初始值
-            if(res.Evns[i]){
-                switch(res.Evns[i].like){
-                    case 0: for(let j=0;j < res.Evn.length;j++){
-                            if($(this).dataset.id == res.Evns[i].eventId){
-                                $('.btnpick')[j].dataset.like = res.Evns[i].like.toString();
-                            }
-                    }
-                }
-            }
-        }
-        pick();
-    },function(err){
-        console.log(err);
-    });
+        });
+        $('#mainbefore').after(cardbox);
+        console.log(eventId);
+        //获取刷新页面后点赞的初始值
+    //         if(res.Evns[i]){
+    //             switch(res.Evns[i].like){
+    //                 case 0: for(let j=0;j < res.Evn.length;j++){
+    //                         if($(this).dataset.id == res.Evns[i].eventId){
+    //                             $('.btnpick')[j].dataset.like = res.Evns[i].like.toString();
+    //                         }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     pick();
+
+    },err => {
+        console.log('xxxx',err)
+    })
+    //交互
+    // http.post('/fnc/unclaimed',{},function(res){
+    //     for(let i=0;i < res.Evn.length;i++){
+    //         var a = parseInt(res.Evn[i].date);
+    //         var time = new Date(a);
+    //         var tag =res.Evn[i].tag;
+    //         var month =time.getMonth();
+    //         var day =time.getDay();
+    //         var hour = time.getHours();
+    //         var minutes = time.getMinutes();
+    //         var describe =res.Evn[i].describe;
+    //         var eventId = res.Evn[i].eventId;
+    //         var cardbox = `
+    //         <div class="mainbox">
+    //             <div class="blackbox"></div>
+    //             <div class="mainImg" data-id = "${eventId}">
+    //                 <div class="mainboxtop">
+    //                     <div class="label">${tag}</div>
+    //                     <div class="date">${month}月${day}日</div>
+    //                     <div class="time">${hour}:${minutes}</div>
+    //                 </div>
+    //                 <div class="mainboxmid">
+    //                     <p>${describe}</p>
+    //                 </div>
+    //             </div>
+    
+    //             <div class="mainboxbottom">
+    //                 <div class="mainhead"><div class="headimg"></div><span>jio雯婷</span></span></div>
+    //                 <div class="btnPick" data-id = "${eventId}" data-like ="0"></div>
+    //                 <div class="btnDiscuss"></div>
+    //             </div>
+    //         </div>`;
+    //         $('#mainbefore').after(cardbox);
+    //         console.log(eventId);
+    //         //获取刷新页面后点赞的初始值
+    //         if(res.Evns[i]){
+    //             switch(res.Evns[i].like){
+    //                 case 0: for(let j=0;j < res.Evn.length;j++){
+    //                         if($(this).dataset.id == res.Evns[i].eventId){
+    //                             $('.btnpick')[j].dataset.like = res.Evns[i].like.toString();
+    //                         }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     pick();
+    // },function(err){
+    //     console.log(err);
+    // });
 
 
     function pick(){
@@ -81,6 +128,8 @@ $('document').ready(function(){
         $('#main').css('display','none');
         $('#carddetail').css('display','block');
         $('#carddetail').css('opacity','1');
+        //交互
+        
     });
     $('.btnDiscuss').click(function(){
         $('#header').css('display','none');
